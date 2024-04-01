c2534a8cce2fd6c2e318d659d897cb802c95349c
Adam-Abera <iruleadamabera@gmail.com>
2024-04-02 01:55:31 +0300

    Added files

---
+++ client/src/pages/admin/Layout.js
@@ -0,0 +1,30 @@
+import { Outlet, Link, useLocation } from "react-router-dom";
+
+const Layout = () => {
+
+  const location = useLocation();
+  //const {email} = location.state;
+  const email = document.cookie.split(';')[0].split('=')[1].replaceAll('"','');
+  return (
+    <>
+     <br/> <br/> 
+      <nav>
+
+        <ul style={{backgroundColor:"#11676d"}}>
+          <li style={{display:"inline", marginRight:"30px", color: "orange"}}>Quick Links: </li>
+          <li style={{display:"inline", marginRight:"950px"}}>
+            <Link to="/admin" style={{color: "orange"}} state={{email:email}}>Admin</Link>
+          </li>
+          <li style={{display:"inline"}}>
+          <Link to="/" style={{ backgroundColor:"orange", color: "black"}}
+          className="btn btn-primary">Back to Home</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Outlet />
+    </>
+  )
+};
+
+export default Layout;
